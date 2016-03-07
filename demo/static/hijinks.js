1e552570c8d91d38b5e3eacc59d68619619185cd
Ruaidhri Devery <info@fluid.ie>
2016-03-06 23:39:44 -0800

    housekeeping

--- demo/static/hijinks.js
+++ demo/static/hijinks.js
@@ -1,28 +1,31 @@
-(function () {
+(function() {
 
   function hijinksResponse() {
-    var content = this.responseText;
-    var el = document.createElement("div");
-    var id, child, old, i, tagName;
+    var temp = document.createElement("div"),
+        dup = [],
+        id, child, old, i, tagName;
+
     // assign HTML response to a temporary container
-    el.innerHTML = content;
-    var dup = [];
-    for (i = 0; i < el.children.length; i++) {
-      dup[i] = el.children[i]
+    temp.innerHTML = this.responseText;
+    for (i = 0; i < temp.children.length; i++) {
+      dup[i] = temp.children[i]
     };
 
     for (i = 0; i < dup.length; i++) {
       old = null;
       child = dup[i];
       tagName = child.tagName.toLowerCase()
-
       if (["html", "head", "title", "body"].indexOf(tagName) > -1) {
+        // this node is one of the singleton tags
         old = document.getElementsByTagName(tagName)[0];
       } else if (child.hasAttribute("id")) {
+        // This node has an id which much be unique on the page.
         id = child.getAttribute("id");
         if (id) {
           old = document.getElementById(id);
         }
+      } else {
+        // ignore this node
       }
       if (old) {
         old.parentElement.replaceChild(child, old);
@@ -32,11 +35,13 @@
 
   function documentClick(e) {
     e = e || event;
-    var from = findParent('a',e.target || e.srcElement);
-    if (from && from.hasAttribute("hijinks")){
+    var from = findParent('a', e.target || e.srcElement);
+    if (from && from.hasAttribute("hijinks")) {
       var url = from.getAttribute('href');
       if (history) {
-        history.pushState({url: url}, null, url);
+        history.pushState({
+          url: url
+        }, null, url);
       }
       loadHijinksPartial(url)
       event.preventDefault();
@@ -51,39 +56,39 @@
     oReq.send();
   }
 
-  //find first parent with tagName [tagname]
-  function findParent(tagname,el){
-    if ((el.nodeName || el.tagName).toLowerCase()===tagname.toLowerCase()){
+  function findParent(tagname, el) {
+    if ((el.nodeName || el.tagName).toLowerCase() === tagname.toLowerCase()) {
       return el;
     }
-    while (el = el.parentNode){
-      if ((el.nodeName || el.tagName).toLowerCase()===tagname.toLowerCase()){
+    while (el = el.parentNode) {
+      if ((el.nodeName || el.tagName).toLowerCase() === tagname.toLowerCase()) {
         return el;
       }
     }
     return null;
   }
 
-
-  var el = document;
-
-  if (el.addEventListener) {
-    el.addEventListener('click', documentClick, false);
-  } else if (el.attachEvent)  {
-    el.attachEvent('onclick', documentClick);
-  }
-
-  window.onpopstate = function(e){
-      if(e.state.url){
-          loadHijinksPartial(e.state.url);
-      }
+  window.onpopstate = function(e) {
+    if (e.state.url) {
+      loadHijinksPartial(e.state.url);
+    }
   };
 
-  if (history) {
-    (function () {
+  (function() {
+    var el = document;
+
+    if (el.addEventListener) {
+      el.addEventListener('click', documentClick, false);
+    } else if (el.attachEvent) {
+      el.attachEvent('onclick', documentClick);
+    }
+
+    if (history) {
       // set the state so that back button will work properly
       var url = window.location.toLocaleString();
-      history.replaceState({url: url}, document.title);
-    }())
-  }
-}())
+      history.replaceState({
+        url: url
+      }, document.title);
+    }
+  }());
+}());
